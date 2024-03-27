8b454d645676af4d5d044b7572027b1ab9fe1def
Martin Shindarski <martin.shindarski@outlook.com>
2024-03-27 15:02:17 +0200

    clean up

--- src/actions/fetchLatAndLong.js
+++ src/actions/fetchLatAndLong.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 export const fetchLatAndLong = async (city) => {
-  const responce = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${
-      import.meta.env.VITE_API_KEY
-    }`
-  );
+  try {
+    const responce = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
 
-  return responce.data;
+    return responce.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
